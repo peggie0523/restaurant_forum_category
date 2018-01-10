e73bc981d46bb908c4046618d8a9242faa9b1081
peggie0523 <peggie0523@gmail.com>
2018-01-10 21:18:48 +0800

    setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
  root "restaurants#index"
+ resources :restaurants, only: [:index, :show]
 
  namespace :admin do
-    resources :restaurants
+    resources :restaurants, except: [:new, :create, :edit, :update, :destroy]
     resources :categories
     root "restaurants#index"
  end
